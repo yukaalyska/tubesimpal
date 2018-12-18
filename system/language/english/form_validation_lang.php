29af207a92e9d338fdb53001d82ad16e5e862e73
yukaalyska <45929527+yukaalyska@users.noreply.github.com>
2018-12-18 20:48:29 +0700

    Add files via upload

---
+++ system/language/english/form_validation_lang.php
@@ -0,0 +1,25 @@
+<?php
+
+$lang['required']			= "Bagian %s wajib diisi.";
+$lang['isset']				= "Bagian %s harus mempunyai nilai.";
+$lang['valid_email']		= "Bagian %s harus berisi email yang sah.";
+$lang['valid_emails']		= "Bagian %s harus berisi seluruh email yang sah.";
+$lang['valid_url']			= "Bagian %s field must contain a valid URL.";
+$lang['valid_ip']			= "Bagian %s field must contain a valid IP.";
+$lang['min_length']			= "Bagian %s field must be at least %s characters in length.";
+$lang['max_length']			= "Bagian %s field can not exceed %s characters in length.";
+$lang['exact_length']		= "Bagian %s field must be exactly %s characters in length.";
+$lang['alpha']				= "Bagian %s field may only contain alphabetical characters.";
+$lang['alpha_numeric']		= "Bagian %s harus berisi angka huruf.";
+$lang['alpha_dash']			= "Bagian %s field may only contain alpha-numeric characters, underscores, and dashes.";
+$lang['numeric']			= "Bagian %s harus berupa angka.";
+$lang['is_numeric']			= "Bagian %s Harus berisi karakter numerik.";
+$lang['integer']			= "Bagian %s harus berisi bilangan bulat.";
+$lang['regex_match']		= "Bagian %s field is not in Bagian correct format.";
+$lang['matches']			= "Bagian %s tidak sama dengan bagian %s ";
+$lang['is_natural']			= "Bagian %s field must contain only positive numbers.";
+$lang['is_natural_no_zero']	= "Bagian %s field must contain a number greater than zero.";
+
+
+/* End of file form_validation_lang.php */
+/* Location: ./system/language/english/form_validation_lang.php */
